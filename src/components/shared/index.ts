950137e7667abc0b8e8e99fb732ce5c26ef9fca3
imil <imil.net@mail.ru>
2024-07-31 15:59:06 +0500

    0.25percent

---
+++ src/components/shared/index.ts
@@ -0,0 +1,13 @@
+
+export { Categories } from './categories';
+export { Container } from './container';
+export { Header } from './header';
+export { Title } from './title';
+export { SortPopup } from './sortPopup';
+export { TopBar } from './top-bar';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
+export { CheckboxFiltersGroup } from './checkbox-filters-group';
+export { ProductCard } from './product-card';
+export { ProductsGroupList } from './products-group-list';
+export {SearchInput} from './search-input'
